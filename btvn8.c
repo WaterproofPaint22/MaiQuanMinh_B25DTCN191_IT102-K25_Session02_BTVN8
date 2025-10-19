39e7b3a26291f4845277e3e40b2a9330c3eb3ef5
WaterproofPaint22 <maiquanminh220407@gmail.com>
2025-10-19 18:02:09 +0700

    Update btvn8.c

--- btvn8.c
+++ btvn8.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-	int number = 12345, number1, number2;
-	int result = 0 ; 
+	int number = 12345, number1;
+	int result = 0 ;
+	while (number > 0) {
 	number1 = number % 10;
-	number2 = number / 10;
 	result = result * 10 + number1;
+	number2 = number / 10;
+	}
 	printf("\nSo da duoc dao nguoc (result): %d\n", result);
     
     return 0; 
 }
+
